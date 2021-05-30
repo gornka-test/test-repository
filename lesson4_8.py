d65844919484c063e4072dc881a5bbd3e1eb6d26
Karina Gorn <karinagorn@gmail.com>
2021-05-30 13:33:58 +0300

    Урок 4 Задание 8

--- lesson4_8.py
+++ lesson4_8.py
@@ -8,7 +8,7 @@ try:
     browser.get(link)
     wait = WebDriverWait(browser, 10)
 
-    products = browser.find_elements_by_css_selector("li.product")
+    products = browser.find_elements_by_css_selector("li.product") 
     for item in products:
         stickers = item.find_elements_by_css_selector('.sticker')
         assert (len(stickers) == 1)
