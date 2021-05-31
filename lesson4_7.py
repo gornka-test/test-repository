d5df019f6c0aa82521e3e4c0b214a94a011f2fe7
Karina Gorn <karinagorn@gmail.com>
2021-05-31 22:08:17 +0300

    Урок 4 Задание 7

--- lesson4_7.py
+++ lesson4_7.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 try:
     link = "http://localhost/admin/login.php"
@@ -10,12 +14,14 @@ try:
     browser.find_element_by_name("username").send_keys("admin")
     browser.find_element_by_name("password").send_keys("admin")
     browser.find_element_by_name("login").click()
-    wait = WebDriverWait(browser, 10)
+
 
     length_menu = len(browser.find_elements_by_css_selector("ul#box-apps-menu > li"))
     for num_menu in range(length_menu):
         menu_items = browser.find_elements_by_css_selector("ul#box-apps-menu > li")
         menu_items[num_menu].click()
+        browser.implicitly_wait(10)
+        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
 
         # TODO: Для дебага
         time.sleep(3)
@@ -24,6 +30,7 @@ try:
         for num_submenu in range(length_child):
             submenu_items = browser.find_elements_by_css_selector(".docs>li>a")
             submenu_items[num_submenu].click()
+            heading = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
